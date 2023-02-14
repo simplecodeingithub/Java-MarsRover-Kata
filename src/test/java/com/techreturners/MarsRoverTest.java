548022fd63edfe38e236dc88d3b7042500f43df2
chaitra <chaitrachubz456@gmail.com>
2023-02-14 18:40:03 +0000

    Test passes for second rover with move forward command

--- src/test/java/com/techreturners/MarsRoverTest.java
+++ src/test/java/com/techreturners/MarsRoverTest.java
@@ -10,8 +10,15 @@ public class MarsRoverTest {
     Plateau plateau=new Plateau(5,5);
       Rover rover1=new Rover(1,2,'N');
       plateau.addRover(rover1);
+      //rover1.move("M");
       rover1.move("LMLMLMLMM");
       assertEquals("1 3 N",rover1.toString());
-      System.out.println("The position is :" + rover1.toString());
+      System.out.println("The final position is :" + rover1.toString());
+
+      Rover rover2=new Rover(3,3,'E');
+      plateau.addRover(rover2);
+      rover2.move("MMRMMRMRRM");
+      assertEquals("5 1 E",rover2.toString());
+      System.out.println("The final position is :" + rover2.toString());
   }
 }
